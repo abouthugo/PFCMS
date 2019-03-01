71821626e112cba83007c1c19e0db22da1353e62
Cris Lombardo <Daddy@Criss-Air.fios-router.home>
2019-02-28 20:40:25 -0500

    generates shipId and Location

--- AIdummy.js
+++ AIdummy.js
@@ -1,4 +1,15 @@
 // I am AI, I provide data!
+var events = require('events');
+var util = require('util');
+
+//Constructor
+var Ships = function(shipID, shipCoor){
+  this.shipID = shipID;
+  this.shipCoor = shipCoor;
+};
+
+//allows Ships direct call to EventEmitter? Not 100% sure on how to elaborate, EVENT Constructor binded to Ships Constructor
+util.inherits(Ships, events.EventEmitter);
 
 // var location = function () {
 // Setting Map Size
@@ -6,24 +17,27 @@
   let mapY = 100;
 
 // Use MapSize to determine number of ships
-  let shipNums = Math.round(Math.sqrt(mapX + mapY));
+  var shipNums = Math.round(Math.sqrt(mapX + mapY));
 
-  console.log(shipNums);
-for (let i = 0; i < shipNums; i++) {
-  let shipID = i;
-  // assign ship location to shipID, datbase?
-  // shipCoor cannot be equivalent to another shipCoor
+  var shipID = [];
 
-}
+// Generates ships
+for (var i = 0; i < shipNums; i++){
+  //Generate Coordinates for the ships)
+  let shipX = Math.round(Math.random() * mapX);
+  let shipY = Math.round(Math.random() * mapY);
+  let shipCoor = '(' + shipX + ',' + shipY + ')';
 
-//Generate Coordinates for the ships)
-let shipX = Math.round(Math.random() * mapX);
-let shipY = Math.round(Math.random() * mapY);
+// Possibly different way to define id?
+  shipID[i] = new Ships(i + 1, shipCoor);
+};
 
-let shipCoorTest = '(' + shipX + ',' + shipY + ')';
-let shipCoor = '' + shipX + shipY + '';
+//creates an EventEmitter that will print out ship info
+shipID.forEach(function(ships) {
+  ships.on('sendloc', function(mssg){
+    console.log('Ship: ' + ships.shipID + ' @ location: ' + ships.shipCoor);
+  });
+});
 
-
-
-// res.end(shipCoor);
-// }
+//calls specific ship by ID
+shipID[2].emit('sendloc');
