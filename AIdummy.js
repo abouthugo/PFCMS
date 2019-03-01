a91b5f695d6ef675a840e7b26f4862efabd8fd20
lombardoc4 <lombardoc4@montclair.edu>
2019-03-01 13:38:26 -0500

    dynamic function call - run index.js

--- AIdummy.js
+++ AIdummy.js
@@ -3,21 +3,22 @@ var events = require('events');
 var util = require('util');
 
 //Constructor
-var Ships = function(shipID, shipCoor){
+var Ships = function(shipID, shipCoor, shipType){
   this.shipID = shipID;
   this.shipCoor = shipCoor;
+  this.shipType = shipType;
 };
 
 //allows Ships direct call to EventEmitter? Not 100% sure on how to elaborate, EVENT Constructor binded to Ships Constructor
 util.inherits(Ships, events.EventEmitter);
 
-// var location = function () {
+
 // Setting Map Size
   let mapX = 100;
   let mapY = 100;
 
 // Use MapSize to determine number of ships
-  var shipNums = Math.round(Math.sqrt(mapX + mapY));
+  var shipNums = 20 //Math.round(Math.sqrt(mapX + mapY));
 
   var shipID = [];
 
@@ -28,16 +29,47 @@ for (var i = 0; i < shipNums; i++){
   let shipY = Math.round(Math.random() * mapY);
   let shipCoor = '(' + shipX + ',' + shipY + ')';
 
+  //Figures out ship type (Military, Commerical, Unknown)
+  if (i % 2 == 0) {
+    var shipType = 'Commercial';
+  };
+  if (i % 2 == 1 && i <= (shipNums/2)){
+    var shipType = 'Military' ;
+  };
+  if (i % 2 == 1 && i > (shipNums/2)){
+    var shipType = 'Unknown' ;
+  };
+
 // Possibly different way to define id?
-  shipID[i] = new Ships(i + 1, shipCoor);
+  shipID[i] = new Ships(i + 1, shipCoor, shipType);
 };
 
 //creates an EventEmitter that will print out ship info
 shipID.forEach(function(ships) {
   ships.on('sendloc', function(mssg){
-    console.log('Ship: ' + ships.shipID + ' @ location: ' + ships.shipCoor);
+    console.log('Ship: ' + ships.shipID + ' is a ' + ships.shipType + ' @ location: ' + ships.shipCoor);
   });
 });
 
 //calls specific ship by ID
-shipID[2].emit('sendloc');
+exports.threats = function () {
+  shipID.forEach(function(ships){
+    if(ships.shipType == 'Unknown'){
+      ships.emit('sendloc');
+    };
+  });
+};
+exports.support = function () {
+  shipID.forEach(function(ships){
+      if(ships.shipType == 'Military'){
+      ships.emit('sendloc');
+    };
+  });
+};
+exports.passive = function () {
+  shipID.forEach(function(ships){
+    if(ships.shipType == 'Commercial'){
+      ships.emit('sendloc');
+    }
+  });
+};
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+//imports defined exports from AIdummy.js
+var location = require('./AIdummy.js')
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -7,6 +9,19 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 });
+
+//Get location of potential pirates
+console.log('Potential threats: ');
+location.threats();
+
+//Get location of military support
+console.log('Potential support: ');
+location.support();
+
+//Get location of ships that might be attacked
+console.log('Possibly attacked: ');
+location.passive();
+
 
 server.listen(port, hostname);
 console.log(`Server running at http://${hostname}:${port}/`);
